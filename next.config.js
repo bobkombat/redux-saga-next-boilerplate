95bdd0b2530675eca29b00bcaf0a6dddb93ecac4
Ridhwan Ramadhany <rdhwnrmdhny@gmail.com>
2022-02-26 21:59:38 +0700

    Change build to preact Setup Sass Setup tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+const nextConfig = {
+  swcMinify: true,
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
